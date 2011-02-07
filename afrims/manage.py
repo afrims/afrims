5ce70556208fbf6659bdf69776aaf72dff987cb3
Colin Copeland <copelco@caktusgroup.com>
2011-02-07 15:29:36 -0500

    add project directory to Python path; add example test file

--- afrims/manage.py
+++ afrims/manage.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-#import sys, os
-
+import sys, os
+from os.path import exists, join
 from django.core.management import execute_manager
-import settings
+
+# use a default settings module if none was specified on the command line
+DEFAULT_SETTINGS = 'afrims.localsettings'
+DEFAULT_TEST_SETTINGS = 'afrims.test_localsettings'
+settings_specified = any([arg.startswith('--settings=') for arg in sys.argv])
+if not settings_specified and len(sys.argv) >= 2:
+    if sys.argv[1] == 'test':
+        settings = DEFAULT_TEST_SETTINGS
+    else:
+        settings = DEFAULT_SETTINGS
+    print "NOTICE: using default settings module '%s'" % settings    
+    sys.argv.append('--settings=%s' % settings)
 
 
 if __name__ == "__main__":
-#    project_root = os.path.abspath(
-#        os.path.dirname(__file__))
+    # all imports should begin with the full Python package ('afrims.'):
+    project_root = os.path.abspath(os.path.dirname(__file__))
+    # if project_root in sys.path:
+    #     sys.path.remove(project_root)
+    sys.path.insert(0, os.path.dirname(project_root))
 
-#    path = os.path.join(project_root, "apps")
-#    sys.path.insert(0, path)
-
-#    sys.path.insert(0, project_root)
+    from afrims import settings
     execute_manager(settings)
+
--- afrims/settings.py
+++ afrims/settings.py
@@ -139,7 +139,7 @@ TEST_EXCLUDED_APPS = [
 ]
 
 # the project-level url patterns
-ROOT_URLCONF = "urls"
+ROOT_URLCONF = "afrims.urls"
 
 # since we might hit the database from any thread during testing, the
 # in-memory sqlite database isn't sufficient. it spawns a separate
