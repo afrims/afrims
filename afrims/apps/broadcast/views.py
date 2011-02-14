d8ef9cbd89d325ca981e65f5ace10a2a2e730b85
Colin Copeland <copelco@caktusgroup.com>
2011-02-14 10:56:12 -0500

    add queued broadcast list

--- afrims/apps/broadcast/views.py
+++ afrims/apps/broadcast/views.py
@@ -9,7 +9,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from afrims.apps.broadcast.forms import BroadcastForm
-
+from afrims.apps.broadcast.models import Broadcast
 
 @login_required
 @transaction.commit_on_success
@@ -22,9 +22,10 @@ def send_message(request):
             return HttpResponseRedirect(reverse('send-message'))
     else:
         form = BroadcastForm()
-
+    broadcasts = Broadcast.objects.exclude(schedule_frequency='')
     context = {
         'form': form,
+        'broadcasts': broadcasts.order_by('date'),
     }
     return render_to_response('broadcast/send_message.html', context,
                               RequestContext(request))
