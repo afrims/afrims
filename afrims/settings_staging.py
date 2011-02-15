5af7535caf6ed81ccdddc0a8cac04a698fc13652
Colin Copeland <copelco@caktusgroup.com>
2011-02-15 14:55:47 -0500

    update staging settings with db and log file info

--- afrims/settings_staging.py
+++ afrims/settings_staging.py
@@ -1,5 +1,10 @@
 from afrims.settings import *
 
+ADMINS = (
+    ('Development Team', 'afrims-dev@dimagi.com'),
+)
+MANAGERS = ADMINS
+
 INSTALLED_BACKENDS = {
     "message_tester": {
         "ENGINE": "rapidsms.backends.bucket",
@@ -9,12 +14,19 @@ INSTALLED_BACKENDS = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "afrims_staging",
+        "NAME": "afrimsdb",
         "USER": "afrims",
-        "PASSWORD": "",
+        "PASSWORD": "", # provided by localsettings.py
         "HOST": "localhost",
+        "PORT": "5433",
     }
 }
 
 DEBUG = True
+
+LOG_LEVEL = "DEBUG"
+LOG_FILE = '/home/afrims/www/staging/log/router.log'
+LOG_FORMAT = "%(asctime)s %(levelname)-8s - %(name)-26s %(message)s"
+LOG_SIZE = 33554432 # 2^25
+LOG_BACKUPS = 10 # number of logs to keep
 
