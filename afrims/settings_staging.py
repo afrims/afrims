5bbd1fd3d4b4120d6818ad1e2ab9f584a6f67850
Anton de Winter <adewinter@dimagi.com>
2011-02-17 15:34:36 -0500

    added txtNation backend

--- afrims/settings_staging.py
+++ afrims/settings_staging.py
@@ -19,7 +19,14 @@ DATABASES = {
         "PASSWORD": "", # provided by localsettings.py
         "HOST": "localhost",
         "PORT": "5433",
-    }
+    },
+    "txtnation" : {"ENGINE":  "afrims.backends.http",
+                   "host":"0.0.0.0",
+            "port": 9088,
+            "gateway_url": "http://client.txtnation.com/mbill.php",
+            "params_outgoing": "reply=%(reply)s&id=%(id)s&network=%(network)s&number=%(phone_number)s&message=%(message)s&ekey=<SECRET_EKEY>&cc=dimagi&currency=THB&value=0&title=trialcnct",
+            "params_incoming": "action=action&id=%(id)s&number=%(phone_number)s&network=%(network)s&message=%(message)s&shortcode=%(sc)s&country=%(country_code)&billing=%(bill_code)s"
+    },
 }
 
 DEBUG = True
