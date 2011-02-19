702ee99bbd77a002f00f72e30b41f4c0d3ceff9f
Colin Copeland <copelco@caktusgroup.com>
2011-02-19 17:23:45 -0500

    add view that lists message history. refs #16613

--- afrims/apps/broadcast/urls.py
+++ afrims/apps/broadcast/urls.py
@@ -5,5 +5,6 @@ from afrims.apps.broadcast import views
 urlpatterns = patterns('',
     url(r'^send/$', views.send_message, name='send-message'),
     url(r'^schedule/$', views.schedule, name='broadcast-schedule'),
+    url(r'^messages/$', views.messages, name='broadcast-messages'),
 )
 
--- afrims/apps/broadcast/views.py
+++ afrims/apps/broadcast/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Count
 
 from afrims.apps.broadcast.forms import BroadcastForm
-from afrims.apps.broadcast.models import Broadcast
+from afrims.apps.broadcast.models import Broadcast, BroadcastMessage
 
 
 @login_required
@@ -31,7 +31,6 @@ def send_message(request):
                               RequestContext(request))
 
 @login_required
-@transaction.commit_on_success
 def schedule(request):
     broadcasts = Broadcast.objects.exclude(schedule_frequency__isnull=True)
     broadcasts = broadcasts.annotate(recipients=Count('groups__contacts'))
@@ -39,5 +38,15 @@ def schedule(request):
         'broadcasts': broadcasts.order_by('date'),
     }
     return render_to_response('broadcast/schedule.html', context,
+                              RequestContext(request))
+
+
+@login_required
+def messages(request):
+    messages = BroadcastMessage.objects.all()
+    context = {
+        'broadcast_messages': messages,
+    }
+    return render_to_response('broadcast/messages.html', context,
                               RequestContext(request))
 
