fcc4bf881098f71cadb29ae96295d19f7caab9ac
Colin Copeland <copelco@caktusgroup.com>
2011-02-21 10:29:33 -0500

    render form with RequestContext for csrf_token; add comments

--- afrims/apps/test_messager/views.py
+++ afrims/apps/test_messager/views.py
@@ -1,11 +1,13 @@
 from django.utils import simplejson as json
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.template.context import RequestContext
 
 from afrims.apps.test_messager.forms import MessageForm
 
 
 class JsonResponse(HttpResponse):
+    """ Simple HttpResponse extension that converts data into JSON """
     def __init__(self, content='', mimetype=None, status=None,
                  content_type=None):
         content = json.dumps(content)
@@ -15,6 +17,7 @@ class JsonResponse(HttpResponse):
 
 
 def message_form(request):
+    """ AJAX view for rendering test messager form and sending messages """
     if request.POST:
         form = MessageForm(request.POST)
         if form.is_valid():
@@ -29,7 +32,8 @@ def message_form(request):
         if message:
             initial = {'message': message}
         form = MessageForm(initial=initial)   
-    form = render_to_string('test_messager/form.html', {'form': form})
+    form = render_to_string('test_messager/form.html', {'form': form},
+                            RequestContext(request))
     content = {
         'form': form,
     }
