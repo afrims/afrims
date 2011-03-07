67e8e927065b20fef4fe7f4d79418fa3789a1fed
Colin Copeland <copelco@caktusgroup.com>
2011-03-07 11:17:49 -0500

    style "add" buttons with jquery ui

--- afrims/static/js/afrims.js
+++ afrims/static/js/afrims.js
@@ -2,5 +2,7 @@
  * General, shared javascript functionality
  */
 $(document).ready(function() {
-    $('.button').button({text: false});
+    $('a.button').button({text: false});
+    $('a.button.add').button("option", "icons", {primary: 'ui-icon-circle-plus'});
+    $('a.button.add').button("option", "text", true);
 });
