a0241a9004987e55b960b44ec32e6180d4576e9c
Colin Copeland <copelco@caktusgroup.com>
2011-03-07 17:48:58 -0500

    fix group tests with Contact extension data

--- afrims/apps/groups/tests.py
+++ afrims/apps/groups/tests.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.forms.models import model_to_dict
+
+from rapidsms.models import Contact
 
 from afrims.tests.testcases import CreateDataTest
 
@@ -8,19 +11,36 @@ from afrims.apps.groups import forms as group_forms
 
 
 class GroupFormTest(CreateDataTest):
+
+    def create_contact(self, data={}):
+        """ Override super's create_contact to include extension fields """
+        defaults = self._data()
+        defaults.update(data)
+        return Contact.objects.create(**defaults)
+
+    def _data(self, initial_data={}, instance=None):
+        """ Helper function to generate form-like POST data """
+        if instance:
+            data = model_to_dict(instance)
+        else:
+            data = {
+                'first_name': self.random_string(8),
+                'last_name': self.random_string(8),
+                'email': 'test@abc.com',
+                'phone': '1112223333',
+            }
+        data.update(initial_data)
+        return data
+
     def test_create_contact(self):
         """ Test contact creation functionality with form """
         group1 = self.create_group()
         group2 = self.create_group()
-        name = self.random_string(16)
-        data =  {
-            'name': name,
-            'groups': [group1.pk],
-        }
+        data = self._data({'groups': [group1.pk]})
         form = group_forms.ContactForm(data)
         self.assertTrue(form.is_valid())
         contact = form.save()
-        self.assertEqual(contact.name, name)
+        self.assertEqual(contact.first_name, data['first_name'])
         self.assertEqual(contact.groups.count(), 1)
         self.assertTrue(contact.groups.filter(pk=group1.pk).exists())
         self.assertFalse(contact.groups.filter(pk=group2.pk).exists())
@@ -31,12 +51,9 @@ class GroupFormTest(CreateDataTest):
         group2 = self.create_group()
         contact = self.create_contact()
         contact.groups.add(group1)
-        data =  {
-            'name': contact.name,
-            'groups': [group2.pk],
-        }
+        data = self._data({'groups': [group2.pk]}, instance=contact)
         form = group_forms.ContactForm(data, instance=contact)
-        self.assertTrue(form.is_valid())
+        self.assertTrue(form.is_valid(), dict(form.errors))
         contact = form.save()
         self.assertEqual(contact.groups.count(), 1)
         self.assertFalse(contact.groups.filter(pk=group1.pk).exists())
