7ae8adc53d4250ddf962e7b013078cf6b725d455
Colin Copeland <copelco@caktusgroup.com>
2011-03-07 19:17:32 -0500

    add default_contact to Contact model; create/associate connections with contacts on form save

--- afrims/apps/groups/extensions/rapidsms/contact.py
+++ afrims/apps/groups/extensions/rapidsms/contact.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 
+from rapidsms.models import Backend
+
 
 class ContactExtra(models.Model):
     """ Abstract model to extend the RapidSMS Contact model """
@@ -11,6 +13,14 @@ class ContactExtra(models.Model):
     email = models.EmailField(blank=True)
     phone = models.CharField(max_length=32, blank=True)
     title = models.CharField(max_length=64, blank=True)
+    default_backend = models.ForeignKey(Backend, null=True, blank=True)
+
+    @property
+    def default_connection(self):
+        try:
+            return self.connection_set.filter(backend=self.default_backend)[0]
+        except IndexError:
+            return None
 
     def save(self, **kwargs):
         self.name = "%s %s" % (self.first_name, self.last_name)
--- afrims/apps/groups/tests.py
+++ afrims/apps/groups/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.forms.models import model_to_dict
 
-from rapidsms.models import Contact
+from rapidsms.models import Contact, Backend, Connection
 
 from afrims.tests.testcases import CreateDataTest
 
@@ -58,3 +58,23 @@ class GroupFormTest(CreateDataTest):
         self.assertEqual(contact.groups.count(), 1)
         self.assertFalse(contact.groups.filter(pk=group1.pk).exists())
         self.assertTrue(contact.groups.filter(pk=group2.pk).exists())
+
+    def test_backend_with_create(self):
+        """ New contacts should be associated with a connection """
+        backend = self.create_backend()
+        data = self._data({'default_backend': backend.pk})
+        form = group_forms.ContactForm(data)
+        self.assertTrue(form.is_valid())
+        contact = form.save()
+        self.assertEqual(contact.default_connection.backend.pk, backend.pk)
+
+    def test_backend_with_existing_connection(self):
+        """ New contacts should auto associate with existing connections """
+        backend = self.create_backend()
+        connection = self.create_connection({'backend': backend})
+        data = self._data({'default_backend': backend.pk,
+                           'phone': connection.identity})
+        form = group_forms.ContactForm(data)
+        self.assertTrue(form.is_valid())
+        contact = form.save()
+        self.assertEqual(contact.default_connection.pk, connection.pk)
