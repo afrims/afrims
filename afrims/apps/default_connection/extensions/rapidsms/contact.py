8316f2415aa884b81a96e8e7bd40291ced432d55
Colin Copeland <copelco@caktusgroup.com>
2011-03-11 14:10:21 -0500

    add logging to connection search

--- afrims/apps/default_connection/extensions/rapidsms/contact.py
+++ afrims/apps/default_connection/extensions/rapidsms/contact.py
@@ -1,5 +1,10 @@
+import logging
+
 from django.db import models
 from django.conf import settings
+
+
+logger = logging.getLogger('afrims.apps.default_connection')
 
 
 class ContactExtra(models.Model):
@@ -21,14 +26,19 @@ class ContactExtra(models.Model):
         from rapidsms.models import Backend
         if not backend_name and hasattr(settings, 'PRIMARY_BACKEND'):
             backend_name = settings.PRIMARY_BACKEND
+            logging.debug('found {0} in settings'.format(backend_name))
         if self.primary_backend_id:
             backend = self.primary_backend
+            logging.debug('using contact backend: {0}'.format(backend))
         elif backend_name:
             backend = Backend.objects.get(name=backend_name)
+            logging.debug('using backend: {0}'.format(backend))
         else:
             backend = Backend.objects.all()[0]
+            logging.debug('using random backend: {0}'.format(backend))
         connection = self.get_or_create_connection(backend=backend)
         if not connection.contact:
+            logging.debug('associating connection to contact')
             connection.contact = self
             connection.save()
         return connection
