f495c12d650074fc276a7b0fd5b9b17217b81aa1
Colin Copeland <copelco@caktusgroup.com>
2011-03-15 15:51:06 -0400

    add full stack test

--- afrims/apps/catch_all/tests.py
+++ afrims/apps/catch_all/tests.py
@@ -2,15 +2,30 @@
 Tests for the Catch All app.
 """
 
+import logging
+
+from rapidsms.tests.scripted import TestScript
 from rapidsms.tests.harness import MockRouter
 from rapidsms.messages.incoming import IncomingMessage
+from rapidsms.apps.base import AppBase
+from rapidsms.models import Backend
 
 from afrims.tests.testcases import CreateDataTest
 
 from afrims.apps.catch_all.app import CatchAllApp
 
 
-class CatchAllTest(CreateDataTest):
+class OtherApp(AppBase):
+
+    name = 'other-app'
+
+    def handle(self, msg):
+        if msg.text == 'other-app-should-catch':
+            msg.respond('caught')
+            return True
+
+
+class CatchAllDefaultTest(CreateDataTest):
 
     def setUp(self):
         self.router = MockRouter()
@@ -27,3 +42,19 @@ class CatchAllTest(CreateDataTest):
         msg = self._send(connection, 'uncaught-message-test')
         self.assertEqual(len(msg.responses), 1)
         self.assertEqual(msg.responses[0].text, self.app.template)
+
+
+class CatchAllTest(TestScriped, CreateDataTest):
+
+    def test_full_stack(self):
+        """ Test catch all functionality alongside other apps """
+        self.router.apps = [OtherApp(self.router), CatchAllApp(self.router)]
+        self.startRouter()
+        backend = self.create_backend(data={'name': 'mockbackend'})
+        self.router.logger.setLevel(logging.DEBUG)
+        self.runScript("""1112223333 > other-app-should-catch
+                          1112223333 < caught
+                          1112223333 > uncaught-message-test
+                          1112223333 > %s""" % CatchAllApp.template)
+        self.stopRouter()
+
