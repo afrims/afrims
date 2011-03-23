573f50b9704e518bfa7a4605a3970dfd7077847f
Colin Copeland <copelco@caktusgroup.com>
2011-03-23 10:01:50 -0400

    add more logging

--- afrims/apps/reminders/importer.py
+++ afrims/apps/reminders/importer.py
@@ -33,9 +33,12 @@ def parse_patient_list(payload):
     Parse entire XML payload sent from external database. All exceptions will 
     be caught, logged, and the database will be rolled back
     """
+    logger.debug('Parsing payload #{0}'.format(payload.pk))
     try:
         root = etree.fromstring(payload.raw_data)
+        logger.debug('Parsed payload #{0}'.format(payload.pk))
         for patient in root.iter("Table"):
+            logger.debug('Found new patient node')
             parse_patient(patient, payload)
     except ValidationError as e:
         logger.exception(e)
@@ -52,8 +55,6 @@ def parse_patient(node, payload):
     but we don't understand it or there was some other error).
     Returns True upon succesfully creating a Patient and related Contact
     '''
-    logger.debug('Creating patient...')
-    logger.debug(etree.tostring(node, pretty_print=True))
     # mapping of XML tags to Patient model fields
     valid_field_names = {
         "Subject_Number": 'subject_number',
@@ -67,22 +68,24 @@ def parse_patient(node, payload):
     for field in node.getchildren():
         key = valid_field_names.get(field.tag, field.tag)
         data[key] = field.text
-    logger.debug(data)
     # look up patient by subject number to see if this is an update
+    subject = data.get('subject_number', '')
     instance = None
-    if 'subject_number' in data:
-        patient_kwargs = {'subject_number': data['subject_number']}
+    if subject:
+        logger.debug('Parsed patient {0}'.format(subject))
         try:
-            instance = reminders.Patient.objects.get(**patient_kwargs)
-            logger.debug("Found patient {0}".format(instance.subject_number))
+            instance = reminders.Patient.objects.get(subject_number=subject)
+            logger.debug("{0} found in database".format(subject))
         except reminders.Patient.DoesNotExist:
-            number = patient_kwargs['subject_number']
-            logger.debug("Patient {0} doesn't exist".format(number))
+            logger.debug("{0} doesn't exist in database".format(subject))
     # construct model form and see if data is valid
     form = PatientForm(data, instance=instance)
     if form.is_valid():
+        logger.debug('{0} data is valid'.format(subject))
         patient = form.save(payload=payload)
+        logger.debug('Saved patient {0}'.format(subject))
     else:
         logger.debug('Patient data is invalid')
         errors = dict((k, v[0]) for k, v in form.errors.items())
+        errors['POST'] = unicode(data)
         raise ValidationError(errors)
