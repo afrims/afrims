3d1061038fcdb7dc385ccc4c0097758d7cf7f10a
Mark Lavin <markdlavin@gmail.com>
2011-04-06 13:49:18 -0400

    JS to keep reminder tab active on sub-nav pages.

--- afrims/apps/reminders/static/reminders/js/reminders.js
+++ afrims/apps/reminders/static/reminders/js/reminders.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
     $('input[name$="time_of_day"]').timepicker({});
     $('input.datepicker').datepicker();
     $('.form-action input[type=submit]').button();
+    $('#tabs li.app-appointmentreminders').addClass('active');
 });
