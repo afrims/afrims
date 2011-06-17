87e2d7cce9543bf2f71e6833a2471cc10c15a9ae
Mark Lavin <mlavin@caktusgroup.com>
2011-06-17 17:11:22 -0400

    Initial backend for TxtNation.

--- afrims/settings_staging.py
+++ afrims/settings_staging.py
@@ -9,13 +9,22 @@ INSTALLED_BACKENDS = {
     "message_tester": {
         "ENGINE": "rapidsms.backends.bucket",
     },
-    "txtnation" : {"ENGINE":  "afrims.backends.http",
-                   "host":"0.0.0.0",
-            "port": 9088,
-            "gateway_url": "http://client.txtnation.com/mbill.php",
-            "params_outgoing": "reply=%(reply)s&id=%(id)s&network=%(network)s&number=%(phone_number)s&message=%(message)s&ekey=<SECRET_EKEY>&cc=dimagi&currency=THB&value=0&title=trialcnct",
-            "params_incoming": "action=action&id=%(id)s&number=%(phone_number)s&network=%(network)s&message=%(message)s&shortcode=%(sc)s&country=%(country_code)&billing=%(bill_code)s"
+    "txtnation" : {
+        "ENGINE":  "afrims.backends.txtnation",
+        "port": '9087',
+        "config": {
+            'ekey': '', # In local settings
+            'cc': 'dimagi',
+            'currency': 'THB',
+        }
     },
+#    "txtnation" : {"ENGINE":  "afrims.backends.http",
+#                   "host":"0.0.0.0",
+#            "port": 9088,
+#            "gateway_url": "http://client.txtnation.com/mbill.php",
+#            "params_outgoing": "reply=%(reply)s&id=%(id)s&network=%(network)s&number=%(phone_number)s&message=%(message)s&ekey=<SECRET_EKEY>&cc=dimagi&currency=THB&value=0&title=trialcnct",
+#            "params_incoming": "action=action&id=%(id)s&number=%(phone_number)s&network=%(network)s&message=%(message)s&shortcode=%(sc)s&country=%(country_code)&billing=%(bill_code)s"
+#    },
     "twilio": {
         "ENGINE": "rtwilio.backend",
         'host': '173.203.221.48', 'port': '8081',
