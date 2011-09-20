bb9b13f7a4e27ef9fed1f74ba0c3d176fc3adb0b
Anton de Winter <adewinter@dimagi.com>
2011-09-20 21:36:35 +0700

    Removed couchlog context processor (uneeded in the presence of staticfiles)

--- afrims/settings.py
+++ afrims/settings.py
@@ -145,7 +145,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     #this is for a custom logo on the dashboard (see LOGO_*_URL in settings, above)
     "rapidsms.context_processors.logo",
     "afrims.apps.reminders.context_processors.messages",
-    "couchlog.context_processors.static_workaround"
+#    "couchlog.context_processors.static_workaround"
 ]
 
 
