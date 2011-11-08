b07bfe372b1b502286b87fb9c64e5fc70aeb49b3
Anton de Winter <adewinter@dimagi.com>
2011-11-08 18:07:11 -0500

    Pre-emptively changed encoding to UTF-8 for further txtNation testing.
    Signed-off-by: Anton de Winter <adewinter@dimagi.com>

--- afrims/backends/txtnation.py
+++ afrims/backends/txtnation.py
@@ -16,7 +16,7 @@ class TxtNationImproperlyConfigured(ImproperlyConfigured):
 
 class TxtNationBackend(RapidHttpBackend):
     default_timeout = 8
-    encoding = 'UTF-16'
+    encoding = 'UTF-8'
 
     def configure(self, host="localhost", port=8080, config=None, **kwargs):
         if "params_incoming" not in kwargs:
