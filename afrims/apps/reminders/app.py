1f15fdb60d2f43ebabbd8aaf4b0da1a1565e7956
Anton de Winter <adewinter@dimagi.com>
2011-11-09 15:25:30 -0500

    Allow months of the year to be localized in {date} translation string for daily appt reminder txt messages
    Signed-off-by: Anton de Winter <adewinter@dimagi.com>

--- afrims/apps/reminders/app.py
+++ afrims/apps/reminders/app.py
@@ -76,7 +76,8 @@ class RemindersApp(AppBase):
 
     pin_regex = re.compile(r'^\d{4,6}$')
     conf_keyword = '1'
-    date_format = '%B %d, %Y'
+#    date_format = '%B %d, %Y'
+    date_format = '%B'
 
     future_appt_msg = _('You have an upcoming appointment in '
                         '{days} days, on {date}. Please reply with '
@@ -136,9 +137,11 @@ class RemindersApp(AppBase):
         num_days = (appt_date - datetime.date.today()).days
         if confirm_response is None:
             confirm_response = cls.conf_keyword
+        date = appt_date.strftime(cls.date_format)
+        date = ugettext(date)
         msg_data = {
             'days': num_days,
-            'date': appt_date.strftime(cls.date_format),
+            'date': '%s %s' % (date ,appt_date.strftime('%d, %Y')),
             'confirm_response': confirm_response,
         }
         if num_days == 0:
@@ -162,6 +165,7 @@ class RemindersApp(AppBase):
         msg_parts = msg.text.split()
         if not msg_parts:
             return False
+
         response = msg_parts[0]
         if response != self.conf_keyword and not self.pin_regex.match(response):
             return False
