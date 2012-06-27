62162ddfb35cccb228e5368c844e73049f0d0c93
Mark Lavin <mlavin@caktusgroup.com>
2012-06-27 09:03:30 -0400

    Cleaning up url patterns.

--- afrims/apps/reports/urls.py
+++ afrims/apps/reports/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
+    url(r'^$', 'afrims.apps.reports.views.dashboard', name='rapidsms-dashboard'),
     url(r'^graphs/system-usage/$', 'afrims.apps.reports.views.system_usage', name='report-system-usage'),
     url(r'^graphs/reminder-usage/$', 'afrims.apps.reports.views.reminder_usage', name='report-reminder-usage'),
 )
