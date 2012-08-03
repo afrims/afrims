8b00bdbee452f34d79cf0b518c9d5f9943cba9bf
Mark Lavin <mlavin@caktusgroup.com>
2012-08-03 09:38:04 -0400

    Additional admin customizations for decisiontree. Better filtering for some existing models. Remove models which don't need to be changed via the admin.

--- afrims/apps/overrides/admin.py
+++ afrims/apps/overrides/admin.py
@@ -7,6 +7,15 @@ from decisiontree import models
 
 # Change tree registration to be more helpful
 admin.site.unregister(models.Tree)
+admin.site.unregister(models.Question)
+admin.site.unregister(models.TreeState)
+admin.site.unregister(models.Answer)
+admin.site.unregister(models.Session)
+# I don't think we have a need for these so let's not clutter the admin
+admin.site.unregister(models.Tag)
+admin.site.unregister(models.TagNotification)
+# There isn't a good use-case for modifying user's answers
+admin.site.unregister(models.Entry)
 
 
 class TreeAdmin(admin.ModelAdmin):
@@ -22,4 +31,58 @@ class TreeAdmin(admin.ModelAdmin):
     report_data.short_description = u"Results Report"
 
 
+class StateInline(admin.StackedInline):
+    "Allow adding a tree state when adding a question."
+
+    model = models.TreeState
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    "Edit questions with inlines to TreeStates."
+
+    inlines = (StateInline, )
+
+
+class TransistionInline(admin.StackedInline):
+    "Allow adding a transistion when adding an answer."
+
+    model = models.Transition
+    extra = 0
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    "Edit answers with inlines to answers."
+
+    list_display = ('name', 'type', 'answer', )
+    list_filter = ('type', )
+    inlines = (TransistionInline, )
+
+
+class StateTransistionInline(TransistionInline):
+    "Transision has more than FK to state so set fk_name."
+    fk_name = 'current_state'
+
+
+class StateAdmin(admin.ModelAdmin):
+    "Edit states with inlines to transisions."
+    
+    list_display = ('name', 'question', )
+    inlines = (StateTransistionInline, )
+
+
+class SessionAdmin(admin.ModelAdmin):
+    "List sessions with better filtering."
+    list_display = ('connection', 'tree', 'current_state', 'start_date', 'last_modified', )
+    list_filter = ('start_date', 'last_modified', 'tree', )
+
+    def current_state(self, obj):
+        "Display current question or note if they have completed the questions."
+        return obj.state if obj.state else "completed"
+    
+
 admin.site.register(models.Tree, TreeAdmin)
+admin.site.register(models.Question, QuestionAdmin)
+admin.site.register(models.Answer, AnswerAdmin)
+admin.site.register(models.TreeState, StateAdmin)
+admin.site.register(models.Session, SessionAdmin)
